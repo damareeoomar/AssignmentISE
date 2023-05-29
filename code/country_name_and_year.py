34a0168c5ebdba6ff2056c0ff5a85edbb7dc03f7
Damaree <21564419@student.curtin.edu.au>
2023-05-29 16:30:01 +0400

    fixing an error in the season code

--- code/country_name_and_year.py
+++ code/country_name_and_year.py
@@ -21,59 +21,6 @@ def Name_Month():
     print(country," ",month)
 
 
-"""
-def season1(country,month):
-    if country == "AUSTRALIA":
-        choice = int(input("Do you want The traditional seasons(1) or the Noongar Seasons(2):  "))
-        if choice == 1 :
-            if month == 12 or month ==1 :
-                season = "Birak"
-            elif month == 2 or month ==3 :
-                season = "Bunuru"
-            elif month == 4 or month == 5 :
-                season = "Djeran"
-            elif month == 6 or month == 7 :
-                season = "Makuru"
-            elif month == 8 or month == 9 :
-                season = "Dijilba"
-            else:
-                season = "Kambarang"
-        else:
-            if month == 12 or 0<month<3 :
-                season = "Summer"
-            elif 2 < month < 6 :
-                season = "Autumn"
-            elif 5 < month < 9:
-                season = "Winter"
-            else:
-                season = "Spring"
-    elif country == 'MAURITUS':
-        if 10 < month < 13   or 0 < month < 5:
-            season = "Summer"
-        elif month == 5 :
-            season = "Autumn"
-        elif 5 < month < 10:
-            season = "Spring"
-        else:
-            season = "Winter"
-    elif country =="SPAIN":
-        if month == 12 or 0 <month<3 :
-            season = "Winter"
-        elif 2 < month < 6 :
-            season = "Spring"
-        elif 5 < month < 9:
-            season = "Summer"
-        else:
-            season = "Autumn"
-    else:
-        if 11 < month < 3:
-            season = "Northeast Monsoon"
-        elif 4 < month < 10:
-            season = "Southeast Monsoon"
-        else:
-            season = "Inter-monsoon"
-    return season 
-"""
 def mauritius(month):
     if 10 < month < 13   or 0 < month < 5:
         season = "Summer"
@@ -166,50 +113,7 @@ def image(season):
     plt.imshow(im.imread(season.lower()+".png"))
     plt.title(country +" & "+ season.upper(), fontsize = "17")
     
-
-    """
-    if season == "Winter":
-        plt.imshow(im.imread("winter.png"))
-       
-    elif season == "Summer":
-        plt.imshow(im.imread("summer.png"))
-       
-    elif season == "Autumn":
-        plt.imshow(im.imread("autumn.png"))
-        
-    elif season == "Spring":
-        plt.imshow(im.imread("spring.png"))
-        
-    elif season == "Northeast Monsoon" or season == "Southeast Monsoon":
-        plt.imshow(im.imread("monsoon.png"))
-        
-    elif season == "Inter-monsoon":
-        plt.imshow(im.imread("inter-monsoon.png"))
-       
-    elif season == "Birak":
-        plt.imshow(im.imread("birak.png"))
-        
-    elif season == "Bunuru":
-        plt.imshow(im.imread("bunuru.png"))
-        
-    elif season == "Djeran":
-        plt.imshow(im.imread("djeran.png"))
-        
-    elif season == "Makuru":
-        plt.imshow(im.imread("makuru.png"))
-        
-    elif season == "Kambarang":
-        plt.imshow(im.imread("kambarang.png"))
-        
-    else:
-        plt.imshow(im.imread("djilba.png"))
-        
-    plt.title(season.upper(), fontsize = "17")
     plt.show()
-    """
-
-    
-
 
 
 
@@ -217,13 +121,7 @@ if __name__ == "__main__":
 
     
     Name_Month()
-    """
-    a = input("Enter the country name: ")        #a = input("Enter the country name: ").upper()
-    country = a.upper()
-    
-    b = input("Enter the number of the month of the year:(1-12) ")   
-    month = int(b)
-    """
+  
 
     season = location(country,month)
     image(season)
